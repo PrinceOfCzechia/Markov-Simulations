920d1629487d81da09ea7c35ec6985c2c96da97a
Petr Král <kralpet9@fjfi.cvut.cz>
2023-05-04 16:00:01 +0200

    redid the code to match other 'return' programs

--- return_2D.py
+++ return_2D.py
@@ -9,37 +9,39 @@ class Walk:
         self.y = np.array( [ 0 ] )
         self.returns = np.array( [ 0 ] )
 
-    def step( self ):
-        p = rn.random()
-        if p < 0.25:
-            self.x = np.append( self.x, self.x[ -1 ] - 1 )
-            self.y = np.append( self.y, self.y[ -1 ] )
-        elif p < 0.50:
-            self.x = np.append( self.x, self.x[ -1 ] + 1 )
-            self.y = np.append( self.y, self.y[ -1 ] )
-        elif p < 0.75:
-            self.x = np.append( self.x, self.x[ -1 ] )
-            self.y = np.append( self.y, self.y[ -1 ] - 1 )
-        else:
-            self.x = np.append( self.x, self.x[ -1 ] )
-            self.y = np.append( self.y, self.y[ -1 ] + 1 )
+    def step( self, x, y ):
+        self.x = np.append( self.x, self.x[ -1 ] + x )
+        self.y = np.append( self.y, self.y[ -1 ] + y )
         self.t = np.append( self.t, self.t[ -1 ] + 1 )
+
+    def eval( self ):
+        p = rn.random()
+        if p < 1/4:
+            self.step( -1, 0 )
+        elif p < 2/4:
+            self.step( 1, 0 )
+        elif p < 3/4:
+            self.step( 0, -1 )
+        else:
+            self.step( 0, 1 )
 
         if self.x[ -1 ] == 0 and self.y[ -1 ] == 0:
             self.returns = np.append( self.returns, self.t[-1] )
 
+
 w = Walk()
-iter = 1e3
+iter = 1e2
 
 for i in range( int(iter) ):
-    w.step()
+    w.eval()
 
 print( 'Returned', w.returns.shape[ 0 ] - 1, 'times' )
 print( 'Return times:', w.returns )
 
 #uncomment to see a plot of the walk
-'''
+
 # simple
+'''
 plt.plot( w.x, w.y, 'bo-' )
 plt.plot( 0, 0, 'ro' )
 plt.show()
